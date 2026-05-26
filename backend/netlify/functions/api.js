f577297129d6d3cf7ab56dc52efd4544a802a5b3
Yatish Mandowara <246053499+yatishmand@users.noreply.github.com>
2026-05-26 16:24:07 +0530

    Optimize serverless helper by disabling callbackWaitsForEmptyEventLoop

--- backend/netlify/functions/api.js
+++ backend/netlify/functions/api.js
@@ -4,6 +4,7 @@ const { app, connectDB } = require('../../app');
 const handler = serverless(app);
 
 exports.handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
   await connectDB();
   return handler(event, context);
 };
